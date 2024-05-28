8102be3ad5f32475557b667ba4e85dccdd59e31a
Ashraf Alhaj <ashrfalhag.11@gmail.com>
2024-05-28 13:05:14 +0200

    feat:maps

--- arrays/main.go
+++ arrays/main.go
@@ -10,4 +10,17 @@ func main() {
 	mySlice := append(myArray, 7)
 
 	fmt.Println(mySlice, cap(mySlice), len(mySlice))
+
+	myMap := map[int8]string{8: "Ash"}
+
+	ash, ok := myMap[8]
+
+	if ok {
+		fmt.Println("We found ash at :", ash)
+		delete(myMap, 8)
+
+		fmt.Println("We deleted ash")
+	} else {
+		fmt.Println("Something went wrong , we can't find a map")
+	}
 }
